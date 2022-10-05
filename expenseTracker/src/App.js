26245d73b0a001985fbfffafcd3e9047e4bcf163
varun325 <sharmavarun1300@gmail.com>
2022-10-05 17:24:39 +0530

    Added NewExpense component for taking user inputs

--- expenseTracker/src/App.js
+++ expenseTracker/src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 function App() {
   const expenses = [
     {
@@ -24,6 +25,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense></NewExpense>
       <Expenses items={expenses} />
     </div>
   );
