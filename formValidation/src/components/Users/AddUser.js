84f8663087c346024ce6c349b231804a9a72126a
varun325 <sharmavarun1300@gmail.com>
2022-10-13 14:32:52 +0530

    Added card and button component

--- formValidation/src/components/Users/AddUser.js
+++ formValidation/src/components/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React from "react";
-
+import Card from "../UI/Card";
+import classes from './AddUser.module.css'
+import Button from "../UI/Button";
 const Adduser = (props) => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
 
   return (
+    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
       <label htmlFor="username">Username</label>
       <input id="username" type="text" />
       <label htmlFor="age">Age (Years!)</label>
       <input id="age" type="number" />
-      <button type="submit">Add User</button>
+      <Button type="submit">Add User</Button>
     </form>
+    </Card>
   );
 };
 
