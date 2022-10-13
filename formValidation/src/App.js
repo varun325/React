f87ae6c6e92bfc27372bb714ef01be3f0ee6cb08
varun325 <sharmavarun1300@gmail.com>
2022-10-13 14:13:04 +0530

    added the Add User component

---
+++ formValidation/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Adduser from './components/Users/AddUser';
+
+
+function App() {
+  return (
+    <div>
+      <Adduser></Adduser>
+    </div>
+  );
+}
+
+export default App;
